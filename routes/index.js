3c533d7e79009b362e1602b217b3872ef2c468c9
Osamu Kodama <deep86.osa@gmail.com>
2018-11-18 23:46:55 +0900

    node-postgresの実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const {Client} = require('pg');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',(req,res,next) => {
+  let response = res;
+  const client = new Client({
+    host:'localhost',
+    database:'node_test_db',
+    user:'postgres',
+    password:'postgres',
+    port:'5432'
+  });
+
+  //接続
+  client.connect();
+
+  //クエリ実行
+  client.query('select * from users',(err,res) => {
+    let data = {
+      title:'Hello World!',
+      rows:res.rows
+    };
+    client.end();
+    console.log(data);
+    response.render('index',data);
+  });
 });
 
 module.exports = router;
